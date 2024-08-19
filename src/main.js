b1e654242bc51a8db324faac4a1670ea366f4f86
LinZeyuan <903586678@qq.com>
2024-08-19 11:38:08 +0800

    basic_layout_commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+// 定义特性标志
+window.__VUE_PROD_DEVTOOLS__ = false;
+window.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
+
+const app = createApp(App);
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
